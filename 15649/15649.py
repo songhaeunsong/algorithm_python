20cb745ac95e0f5905592a460bda0da7d7d679c7
songhaeunsong <gkdms3469@naver.com>
2024-05-16 15:39:08 +0900

    Sol: baekjoon 15649

---
+++ 15649/15649.py
@@ -0,0 +1,24 @@
+# 4 2
+
+import sys
+n, m = [*map(int, sys.stdin.readline().split())]
+
+
+def backtracking(arr, visited):
+
+    if len(arr) == m:
+        print(" ".join(map(str, arr)))
+        return
+
+    for i in range(1, n+1):
+        if visited[i] == 0:
+            arr.append(i)
+            visited[i] = 1
+            backtracking(arr, visited)
+            arr.pop()
+            visited[i] = 0
+
+
+backtracking([], [0] * (n+1))
+
+
